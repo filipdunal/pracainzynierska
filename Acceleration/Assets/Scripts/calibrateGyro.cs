89417f03f45d43cb00d696fe35a1fa535335ee20
neo938 <neo938@gmail.com>
2019-10-29 23:18:44 +0100

    Dodanie projektu z testem żyroskopu

---
+++ Acceleration/Assets/Scripts/calibrateGyro.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+[RequireComponent(typeof(SpriteRenderer))]
+public class calibrateGyro : MonoBehaviour
+{
+    [SerializeField] MeshRenderer target;
+    SpriteRenderer srend;
+    private void Awake()
+    {
+        srend = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnMouseDown()
+    {
+        Debug.Log("AAAAAAAAAA");
+    }
+}
---
+++ Acceleration/Assets/gyroEuler.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class gyroEuler : MonoBehaviour
+{
+    public Text x;
+    public Text y;
+    public Text z;
+    Quaternion gyroData;
+    Vector3 gyroDataEuler;
+    private void Start()
+    {
+        Screen.orientation = ScreenOrientation.Portrait;
+        Input.gyro.enabled = true;
+    }
+
+    private void Update()
+    {
+        gyroData=Input.gyro.attitude;
+        x.text = System.Math.Round(gyroData.eulerAngles.x, 1).ToString();
+        y.text = System.Math.Round(gyroData.eulerAngles.y, 1).ToString();
+        z.text = System.Math.Round(gyroData.eulerAngles.z, 1).ToString();
+    }
+}
