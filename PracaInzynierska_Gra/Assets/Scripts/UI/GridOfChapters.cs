e42ebf43c1f24bf21d7193e5149a9603517dbdd5
neo938 <neo938@gmail.com>
2020-02-08 23:30:59 +0100

    Dodano ScrollView w menu i stałe proporcje 16:9

---
+++ PracaInzynierska_Gra/Assets/Scripts/UI/GridOfChapters.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridOfChapters : MonoBehaviour
+{
+    public List<GameObject> levels;
+    private void Start()
+    {
+        ShowChapters(); 
+    }
+
+    void ShowChapters()
+    {
+        GameObject newObj;
+        foreach (GameObject lvl in levels)
+        {
+            newObj = (GameObject)Instantiate(lvl, transform);
+        }
+    }
+    
+
+
+}
+
