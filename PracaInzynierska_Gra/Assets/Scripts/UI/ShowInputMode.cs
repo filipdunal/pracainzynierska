9fa118d9f1bf118c6966be6356ddd7c07139c1a6
neo938 <neo938@gmail.com>
2020-02-10 19:53:17 +0100

    Stałe proporcje w obu scenach, Slider view jest pionowymi prostokątami przewijanymi horyzontalnie

---
+++ PracaInzynierska_Gra/Assets/Scripts/UI/ShowInputMode.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowInputMode : MonoBehaviour
+{
+    CustomInputModule customInputModule;
+    Text displayedInput;
+    private void Start()
+    {
+        displayedInput = GetComponent<Text>();
+        customInputModule = GameObject.Find("Custom EventSystem").GetComponent<CustomInputModule>();
+    }
+
+    private void OnGUI()
+    {
+        if(customInputModule.controller==CustomInputModule.Controller.gamepad)
+        {
+            displayedInput.text = "Gamepad mode";
+        }
+        else if(customInputModule.controller == CustomInputModule.Controller.mouse)
+        {
+            displayedInput.text = "Mouse mode";
+        }
+        else if(customInputModule.controller == CustomInputModule.Controller.phone)
+        {
+            displayedInput.text = "Phone mode";
+        }
+        else
+        {
+            displayedInput.text = "??? mode";
+        }
+        
+    }
+}
